81f1fb0b363d41832bbb9b554ae3418b99e2686e
Justin Diehl <justind1110@hotmail.com>
2024-09-05 18:12:06 -0500

    picked out a libray with some customization

--- text2Speech.py
+++ text2Speech.py
@@ -1,4 +1,24 @@
-from gtts import gTTs
-
+#from gtts import gTTS
+import pyttsx3
 import os
+
+'''
+this did not sound as good in my opinion
+def ttsgtts(text,lang,accent):
+	mp3 = gTTS(text = text, lang = lang, tld = accent)
+	filename = 'temp.mp3'
+	mp3.save(os.path.dirname(os.path.realpath(__file__)) + '/aud.mp3')
+'''
+
+#can change voice module i think in pyttsx3 if you download more voices
+def tts(text, voice):
+    eng = pyttsx3.init()
+    eng.setProperty('rate', 125)
+    voices = eng.getProperty('voices')
+    eng.setProperty('voice',voices[voice].id)
+    eng.save_to_file(text, os.path.dirname(os.path.realpath(__file__)) + '/aud.mp3')
+    eng.runAndWait()
+	
+
+tts("ahh why am i gay", 0)
 
